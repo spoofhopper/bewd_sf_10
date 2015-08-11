eb0d941732235d6464d7ea38f72a27a3913f9448
Morgan Holland <morganholland@Morgans-MacBook-Air.local>
2015-08-10 21:24:28 -0700

    submitting love_post and car_lot

--- 04_Ruby_Fundamentals/exercises/love_post.rb
+++ 04_Ruby_Fundamentals/exercises/love_post.rb
@@ -1,13 +1,33 @@
 require 'pry'
-require 'pry-byebug'
+
 
 def get_love_interest
+  puts "What's your interest's name?"
+  love_interest = gets.strip
+  puts "Your love interest is #{love_interest.capitalize}"
+  capture_love_interest_response(love_interest)
 end
 
 def capture_love_interest_response(love_interest)
+  puts "Do you think about #{love_interest}?\n"
+  puts "Answer 'Yes' or 'No' \n"
+  user_answer = gets.strip.downcase
+  #binding.pry
+  get_valid_answer(user_answer, love_interest)
 end
 
 def get_valid_answer(user_answer, love_interest)
+  #binding.pry
+  case user_answer
+  when user_answer = "yes"
+      puts "Call #{love_interest}!"
+    when user_answer = "no"
+      puts "Maybe call #{love_interest} later."
+    else
+      puts "I didn't understand your answer, sorry! \n"
+      puts "Please just answer Yes or No next time"
+      capture_love_interest_response(love_interest)
+  end
 end
 
 ### This is where we call methods to run our script
